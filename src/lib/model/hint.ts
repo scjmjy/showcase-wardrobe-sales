0cca2776805ab7919529c34feded3ce42d8616db
charles_yuan <67080101@qq.com>
2021-11-01 15:34:44 +0800

    Impeplement new popup GUI apperance of swtich cube function.
    Also clean up some warnings in hints.ts file.
    
    Signed-off-by: charles_yuan <67080101@qq.com>

--- src/lib/model/hint.ts
+++ src/lib/model/hint.ts
@@ -1,16 +1,16 @@
 import { JsonClassType, JsonInclude, JsonIncludeType, JsonProperty } from "jackson-js";
 import { Position } from "../scheme";
 
-@JsonInclude({value: JsonIncludeType.NON_NULL})
+@JsonInclude({ value: JsonIncludeType.NON_NULL })
 export class Area {
-    @JsonProperty({value: 'cubeId'})
-    @JsonClassType({type: () => [String]})
+    @JsonProperty({ value: "cubeId" })
+    @JsonClassType({ type: () => [String] })
     cubeId: string; // 单元柜uuid
-    @JsonProperty({value: 'startPoint'})
-    @JsonClassType({type: () => [Position]})
+    @JsonProperty({ value: "startPoint" })
+    @JsonClassType({ type: () => [Position] })
     startPoint: Position; // 开始坐标
-    @JsonProperty({value: 'endPoint'})
-    @JsonClassType({type: () => [Position]})
+    @JsonProperty({ value: "endPoint" })
+    @JsonClassType({ type: () => [Position] })
     endPoint: Position; // 结束坐标
 
     constructor(cubeId: string, startPoint: Position, endPoint: Position) {
@@ -20,13 +20,13 @@ export class Area {
     }
 }
 
-@JsonInclude({value: JsonIncludeType.NON_NULL})
+@JsonInclude({ value: JsonIncludeType.NON_NULL })
 export class CubeAreaHint {
-    @JsonProperty({value: 'id'})
-    @JsonClassType({type: () => [String]})
+    @JsonProperty({ value: "id" })
+    @JsonClassType({ type: () => [String] })
     id: string;
-    @JsonProperty({value: 'areas'})
-    @JsonClassType({type: () => [Array, [Area]]})
+    @JsonProperty({ value: "areas" })
+    @JsonClassType({ type: () => [Array, [Area]] })
     areas: Array<Area>;
 
     constructor(id: string, areas: Array<Area> = []) {
@@ -35,16 +35,16 @@ export class CubeAreaHint {
     }
 }
 
-@JsonInclude({value: JsonIncludeType.NON_NULL})
+@JsonInclude({ value: JsonIncludeType.NON_NULL })
 export class AreaHints {
-    @JsonProperty({value: 'spaceEnough'})
-    @JsonClassType({type: () => [Boolean]})
+    @JsonProperty({ value: "spaceEnough" })
+    @JsonClassType({ type: () => [Boolean] })
     spaceEnough: boolean;
-    @JsonProperty({value: 'cubeHints'})
-    @JsonClassType({type: () => [Array, [CubeAreaHint]]})
+    @JsonProperty({ value: "cubeHints" })
+    @JsonClassType({ type: () => [Array, [CubeAreaHint]] })
     cubeHints: Array<CubeAreaHint>;
 
-    constructor(spaceEnough: boolean = true, cubeHints: Array<CubeAreaHint> = []) {
+    constructor(spaceEnough = true, cubeHints: Array<CubeAreaHint> = []) {
         this.cubeHints = cubeHints;
         this.spaceEnough = spaceEnough;
     }
