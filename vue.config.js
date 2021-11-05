98ecb1172d5dfad3d8e4d223165f5c8f102d6835
jinyuan ming <mjyok12345@gmail.com>
2021-11-05 21:40:37 +0800

    refactor: remove unused code

--- vue.config.js
+++ vue.config.js
@@ -72,18 +72,18 @@ module.exports = {
         config.plugins.delete("prefetch");
 
         // set svg-sprite-loader
-        config.module.rule("svg").exclude.add(resolve("src/assets/icons")).end();
-        config.module
-            .rule("icons")
-            .test(/\.svg$/)
-            .include.add(resolve("src/assets/icons"))
-            .end()
-            .use("svg-sprite-loader")
-            .loader("svg-sprite-loader")
-            .options({
-                symbolId: "icon-[name]",
-            })
-            .end();
+        // config.module.rule("svg").exclude.add(resolve("src/assets/icons")).end();
+        // config.module
+        //     .rule("icons")
+        //     .test(/\.svg$/)
+        //     .include.add(resolve("src/assets/icons"))
+        //     .end()
+        //     .use("svg-sprite-loader")
+        //     .loader("svg-sprite-loader")
+        //     .options({
+        //         symbolId: "icon-[name]",
+        //     })
+        //     .end();
 
         config.when(process.env.NODE_ENV !== "development", (config) => {
             //     config
